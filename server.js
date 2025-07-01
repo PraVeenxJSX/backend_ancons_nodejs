f62d47839335f27e105e53cb00eb0869516f06e4
PraVeenxJSX <160173872+PraVeenxJSX@users.noreply.github.com>
2025-07-01 17:39:30 +0530

    changed server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use('/api/users', userRoutes);
 app.use('/api/users', contactRoutes); // Include the contact route here
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 app.use('/' , (req, res) =>{
